f95f623ba21de0cab93a8093f3a108977e1eb58d
Vijaykumar Maradana <email@vijaykumarmaradana2001@gmail.com>
2022-05-25 12:39:09 +0530

    working.js file Created

---
+++ 2_JavaScript/JS_activities/working.js
@@ -0,0 +1,52 @@
+function real() {
+    console.log("Hey, Im real function");
+}
+
+real();
+
+function real() {
+    console.log("Guyz Im the real function");
+}
+
+
+function real() {
+    console.log("No way, only I can be a real function");
+}
+
+// ===================================================
+
+console.log(real);
+real();
+
+var real = function real() {
+    console.log("Hey, Im real function");
+}
+
+
+real = function real() {
+    console.log("Guyz Im the real function");
+}
+
+
+real = function real() {
+    console.log("No way, only I can be a real function");
+}
+
+// ===================================================
+
+console.log(real);
+
+var real = function real() {
+    console.log("Hey, Im real function");
+}
+
+real();
+
+real = function real() {
+    console.log("Guyz Im the real function");
+}
+
+
+real = function real() {
+    console.log("No way, only I can be a real function");
+}
