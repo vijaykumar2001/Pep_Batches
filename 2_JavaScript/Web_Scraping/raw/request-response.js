9c2525d22ff18d6cc4d50e6f648660a55e2cb815
Vijaykumar Maradana <email@vijaykumarmaradana2001@gmail.com>
2022-05-29 17:15:46 +0530

    Update

--- 2_JavaScript/Web_Scraping/raw/request-response.js
+++ 2_JavaScript/Web_Scraping/raw/request-response.js
@@ -1,13 +1,19 @@
 const request = require("request");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 console.log("Before");
 
-request("https://www.espncricinfo.com/series/indian-premier-league-2022-1298423", cb);
+request("https://www.npmjs.com/package/request", cb); // Async
 function cb(error, response, html) {
     if(error){
         console.log("Some error", error);
     }else{
-        console.log(html);
+        const dom = new JSDOM(html); // dom
+        const document = dom.window.document;
+        let contentElem = document.querySelector("._3f330ce9");
+        let content = contentElem.textContent;
+        console.log(content);
     }
 }
 
