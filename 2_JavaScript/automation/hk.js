7ea9be65c57d00936bfc7b4913b538b5bf88fb65
Vijaykumar Maradana <Vijaykumarmaradana2001@gmail.com>
2022-07-03 16:39:23 +0530

    all questions have been selected

--- 2_JavaScript/automation/hk.js
+++ 2_JavaScript/automation/hk.js
@@ -1,5 +1,8 @@
 const puppeteer = require("puppeteer");
 
+const email = "fesagom584@chatich.com"
+const password = "pepcoding123"
+
 let browserPromise = puppeteer.launch({ headless: false,defaultViewport:null,rgs:['--start-maximized']});
 let page;
 browserPromise.then(function(browserInstance){
@@ -9,6 +12,8 @@ browserPromise.then(function(browserInstance){
     page = newPage;
     let hkPromise = page.goto("https://www.hackerrank.com/");
     return hkPromise;
+}).then(function(){
+    return page.waitForSelector('a[data-event-action="Login"]');
 }).then(function(){
     let logInBtnPromise = page.click('a[data-event-action="Login"]',{
         delay:200,
@@ -21,4 +26,51 @@ browserPromise.then(function(browserInstance){
         delay:200,
     })
     return logInDeveloperPromise;
+}).then(function(){
+    return page.waitForSelector('input[name="username"]');
+}).then(function(){
+    let emailTypedPromise = page.type('input[name="username"]',email);
+    return emailTypedPromise;
+}).then(function(){
+    console.log("Email has been typed");
+}).then(function(){
+    return page.waitForSelector('input[name="password"]')
+}).then(function(){
+    let passTypedPromise = page.type('input[name="password"]',password);
+    return passTypedPromise;
+}).then(function(){
+    console.log("Password has been typed");
+    return page.waitForSelector('button[data-analytics="LoginPassword"]');
+}).then(function(){
+    let clickBtnPromise = page.click('button[data-analytics="LoginPassword"]',{
+        delay:200
+    });
+    return clickBtnPromise;
+}).then(function(){
+    console.log("Login ")
+    return page.waitForSelector(".topic-name");
+}).then(function(){
+    let algoBtnClickPromise = page.click(".topic-name");
+    delay: 200
+    return algoBtnClickPromise;
+}).then(function(){
+    console.log("algo button is clicked")
+    return page.waitForSelector('input[value="warmup"]');
+}).then(function(){
+    let warmupClickPromise = page.click('input[value="warmup"]',{
+        delay:200,
+    });
+    return warmupClickPromise;
+}).then(function(){
+    return page.waitForSelector(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled");
+}).then(function(){
+    console.log("warmup has been selected");
+    // querySelectorAll -> $$
+    // querySelector    -> $
+    let allChallengeArrPromise = page.$$(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled",{
+        delay:100
+    })
+    return allChallengeArrPromise;
+}).then(function(allChallengeArr){
+    console.log("Number of questions -> " + allChallengeArr.length);
 })
